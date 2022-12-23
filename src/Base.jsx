5affe4f69fc7c4fa1450eab9b39ffd72c51be452
Deni <06denidzh@gmail.com>
2022-12-23 18:46:22 +0300

    check3

---
+++ src/Base.jsx
@@ -0,0 +1,14 @@
+import dude from '../src/static/img/dude.png'
+
+function Base(){
+return(
+    <div>
+        <h1>
+        welcome
+        </h1>
+        <img src={dude} alt="" />
+    </div>
+)
+}
+    
+    export default Base
